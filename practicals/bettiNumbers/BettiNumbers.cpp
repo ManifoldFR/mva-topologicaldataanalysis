2e7ca8ccba5a6df7d1c12499dde10c5ead6a5405
ManifoldFR <wilson.jallet@gmail.com>
2019-10-22 22:35:43 +0200

    Add practicals C++ code

---
+++ practicals/bettiNumbers/BettiNumbers.cpp
@@ -0,0 +1,15 @@
+#include <BettiNumbers.h>
+
+using namespace std;
+using namespace ttk;
+using namespace bettiNumbers;
+
+BettiNumbers::BettiNumbers() {
+
+  inputData_ = NULL;
+  outputData_ = NULL;
+  triangulation_ = NULL;
+}
+
+BettiNumbers::~BettiNumbers() {
+}
